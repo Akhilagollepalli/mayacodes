97e0a40134ad2ecd0dbe81accca6f9cacadeddf8
Akhila gollepalli <akhilagollepalli@gmail.com>
2025-12-10 11:36:45 +0530

    Add files via upload

---
+++ celsius to fahrenhiet.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	float celsius, Fahrenhiet;
+	printf("Enter Temp in celsius:");
+	scanf("%f, &celsius");
+	Fahrenhiet =(1.8 * celsius) +32;
+	printf(" Enter Temp in Fahrenheit: %.2f",Fahrenheit);
+	return 0;
+}
